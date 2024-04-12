629f7e13346cbe4546c4d327b21be42a58955644
aryo faizi <aryofaizi@gmail.com>
2024-04-12 11:38:08 +0330

    last_four custom templatefilter created and home.html from now shows only last 4 created products in the slider

--- store/templatetags/store_filters.py
+++ store/templatetags/store_filters.py
@@ -3,4 +3,10 @@ register = template.Library()
 
 @register.filter
 def first_image(value):
+    """Gets onely the first image for the products that have multiple images."""
     return value[1].image.url
+
+@register.filter
+def last_four(value):
+    """Represents the last four products created in database."""
+    return value.order_by("-id")[:4]
