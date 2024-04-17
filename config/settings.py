ff8573dc54ac66d913b990519b027cf13340a21e
aryo faizi <aryofaizi@gmail.com>
2024-04-17 16:28:01 +0330

    .env file initialized and settings is now updated based on the .env file

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from django.conf import settings
 from django.core.mail import send_mail
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&ckkoj@qx(mv&j4*j-2^3m@%k2ks0yvfquvujn1a39_(f2_b*('
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda value:[s.strip() for s in value.split(",")])
 
 
 # Application definition
@@ -116,11 +117,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "project",
-        "HOST": "localhost",
-        "USER": "root", 
-        "PASSWORD": "", 
-        'PORT':'3306',
+        'NAME': config("NAME"),
+        "HOST": config("HOST"),
+        "USER": config("USER"), 
+        "PASSWORD": config("PASSWORD"), 
+        'PORT':config("PORT", cast=int),
     }
 }
 
@@ -291,11 +292,11 @@ ACCOUNT_UNIQUE_EMAIL = True
 SITE_ID = 1
 # email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "ericmatthe1@gmail.com"
-EMAIL_HOST_PASSWORD = "fdtx vnrs oevi jkhy"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 
 # media settings
