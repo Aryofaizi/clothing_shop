320b58fa24bd0cb25ba9986c7e69707de983152b
aryo faizi <aryofaizi@gmail.com>
2024-06-19 22:20:39 +0330

    bulk order functionality created and the order: views, order_create.html updated and invoice.html template created in store and the translation files updated based on the need

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import OrderForm
 from cart.cart import Cart
-from .models import OrderItem
+from .models import OrderItem, Order
 
 @login_required
 def order_create_view(request):
@@ -12,32 +12,38 @@ def order_create_view(request):
     if len(cart) == 0:
         return redirect("home_page")
     if request.method == "POST":
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form_obj = form.save(commit=False)
-            form_obj.user = request.user
-            form_obj.save()
+            form = OrderForm(request.POST)
+            if form.is_valid():
+                form_obj = form.save(commit=False)
+                form_obj.user = request.user
+                form_obj.save()
+                
+                for item in cart:
+                    product = item["product_obj"]
+                    size = item["size"]
+                    color = item["color"]
+                    unit_price = int(item["total_price"] / item["quantity"])
+                    OrderItem.objects.create(
+                        order = form_obj,
+                        product = product,
+                        size = size,
+                        color = color,
+                        quantity = item["quantity"],
+                        price = unit_price,
+                    )
+                cart.clear()
+                request.user.first_name = form_obj.first_name
+                request.user.last_name = form_obj.last_name
+                request.user.save()
+                
+                #send order id as session
+                request.session["order_id"] = form_obj.id
+                if "single" in request.POST:
+                    return redirect("payment_process")
+                elif "bulk" in request.POST:
+                    invoice = Order.objects.prefetch_related("items").get(id=form_obj.id)
+                    return render(request, "store/invoice.html", context={"invoice": invoice})
             
-            for item in cart:
-                product = item["product_obj"]
-                size = item["size"]
-                color = item["color"]
-                OrderItem.objects.create(
-                    order = form_obj,
-                    product = product,
-                    size = size,
-                    color = color,
-                    quantity = item["quantity"],
-                    price = int(item["total_price"]),
-                )
-            cart.clear()
-            request.user.first_name = form_obj.first_name
-            request.user.last_name = form_obj.last_name
-            request.user.save()
-            
-            #send order id as session
-            request.session["order_id"] = form_obj.id
-            return redirect("payment_process")
         
     return render(request, "orders/order_create.html",
                   context={"form": form})
